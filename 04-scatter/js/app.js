eb0a685e09b7589f1dc327a1bd0903aabbaaf2ca
Luis F Franco <luisferfranco@gmail.com>
2022-12-06 19:53:47 -0600

    Inicio Scatterplot

---
+++ 04-scatter/js/app.js
@@ -0,0 +1,10 @@
+const graf = d3.select("#graf")
+
+const anchoTotal = +graf.style("width").slice(0, -2)
+const altoTotal = (anchoTotal * 9) / 16
+
+const svg = graf
+  .append("svg")
+  .attr("width", anchoTotal)
+  .attr("height", altoTotal)
+  .attr("class", "fig")
